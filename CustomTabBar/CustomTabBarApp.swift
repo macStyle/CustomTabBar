a8eae329a2c81d7c8767bcc1863c53509cdc5794
Adam <theoutsider009@gmail.com>
2022-09-30 21:52:06 +0200

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Adam Jemni on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
